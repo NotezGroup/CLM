614d7dc2b985a239ae05d4e4602a0c18d337315d
aliyoussefhammoude <ali.hammoude@hotmail.se>
2022-01-13 16:01:04 +0100

    färdig mall

--- src/components/partner/index.js
+++ src/components/partner/index.js
@@ -14,48 +14,48 @@ import Calendar from '../Calendar/CalendarComp'
 
 class PartnerSlider extends Component {
     render() {
-        var settings = {
-            dots: false,
-            infinite: true,
-            speed: 500,
-            arrows: false,
-            slidesToShow: 4,
-            slidesToScroll: 1,
-            autoplay: false,
-            margin: 10,
-            loop: true,
-            responsive: [
-                {
-                  breakpoint: 1200,
-                  settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 1,
-                    infinite: true
-                  }
-                },
-                {
-                  breakpoint: 991,
-                  settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 1
-                  }
-                },
-                {
-                  breakpoint: 768,
-                  settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 1
-                  }
-                },
-                {
-                  breakpoint: 576,
-                  settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                  }
-                }
-              ]
-        };
+        // var settings = {
+        //     dots: false,
+        //     infinite: true,
+        //     speed: 500,
+        //     arrows: false,
+        //     slidesToShow: 4,
+        //     slidesToScroll: 1,
+        //     autoplay: false,
+        //     margin: 10,
+        //     loop: true,
+        //     responsive: [
+        //         {
+        //           breakpoint: 1200,
+        //           settings: {
+        //             slidesToShow: 3,
+        //             slidesToScroll: 1,
+        //             infinite: true
+        //           }
+        //         },
+        //         {
+        //           breakpoint: 991,
+        //           settings: {
+        //             slidesToShow: 3,
+        //             slidesToScroll: 1
+        //           }
+        //         },
+        //         {
+        //           breakpoint: 768,
+        //           settings: {
+        //             slidesToShow: 3,
+        //             slidesToScroll: 1
+        //           }
+        //         },
+        //         {
+        //           breakpoint: 576,
+        //           settings: {
+        //             slidesToShow: 1,
+        //             slidesToScroll: 1
+        //           }
+        //         }
+        //       ]
+        // }
         return (
             // <div className={`partner-sec section-padding ${this.props.partnerclass}`}>
             //     <div className="container">
