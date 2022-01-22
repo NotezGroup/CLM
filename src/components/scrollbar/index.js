471b7355dd5de5c719462222289eb776d84271f6
aliyoussefhammoude <ali.hammoude@hotmail.se>
2022-01-22 22:00:13 +0100

    v.3 without logo

--- src/components/scrollbar/index.js
+++ src/components/scrollbar/index.js
@@ -5,7 +5,7 @@ import './style.css'
 const Scrollbar = () => {
 
     return(
-        <div className="col-lg-12">
+        <div className="scrollIndex col-lg-12">
             <div className="header-menu">
                 <ul className="smothscroll">
                     <li><AnchorLink href='#scrool'><i className="ti-arrow-up"></i></AnchorLink></li>
