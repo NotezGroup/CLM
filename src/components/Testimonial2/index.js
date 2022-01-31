cb146a1c2736b8283fd797fccfe643b8afa65804
aliyoussefhammoude <ali.hammoude@hotmail.se>
2022-01-31 20:33:05 +0100

    klar

--- src/components/Testimonial2/index.js
+++ src/components/Testimonial2/index.js
@@ -19,7 +19,7 @@ class TestSlider2 extends Component {
             margin: 10,
             loop: true,
             autoplay: true,
-            autoplaySpeed: 3000,
+            autoplaySpeed: 5000,
             pauseOnHover: true,
             
         };
